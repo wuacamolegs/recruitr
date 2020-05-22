fc8cb973897ef462f570434466e63e3658fbcf14
camilagsantillan <camila.garciasantillan@increasecard.com>
2020-05-22 13:26:55 +0200

    setup deploy: add heroku url to cors

--- rails-api/config/initializers/cors.rb
+++ rails-api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5000'
+    origins 'http://localhost:5000, https://recruitr-drawbotics-cgs.herokuapp.com/'
 
     resource '*',
       headers: :any,
